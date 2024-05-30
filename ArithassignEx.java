89aef959d6ae2d35d039f4ea64a86efc034eba6c
Bhavana-10 <140387889+Bhavana-10@users.noreply.github.com>
2024-05-30 16:20:46 +0530

    Add files via upload

---
+++ ArithassignEx.java
@@ -0,0 +1,21 @@
+package operators;
+
+public class ArithassignEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10,b=3;
+		a+=b;
+		System.out.println(a);
+		a-=b;
+		System.out.println(a);
+		a*=b;
+		System.out.println(a);
+		a/=b;
+		System.out.println(a);
+		a%=b;
+		System.out.println(a);
+	}
+
+}
